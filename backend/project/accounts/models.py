db1f8bf05d16548de8cfeae67bbd861661e7bd60
koykhaled <koykhaled@gmail.com>
2024-01-07 11:06:47 +0300

    Adding Custom User Model

--- backend/project/accounts/models.py
+++ backend/project/accounts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractBaseUser , PermissionsMixin , BaseUserManager
+
 # Create your models here.
+
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self,email,name,password=None):
+        if not email:
+            raise ValueError('User must have email address')
+        
+        email = self.normalize_email(email) # transform email [Khaled@gmail.com] into [khaled@gmail.com]
+        
+        user = self.model(email=email,name=name)
+        
+        user.set_password(password) # for hashing password
+        
+        user.save()
+        
+        return user
+
+
+class UserAccount(AbstractBaseUser,PermissionsMixin):
+    name = models.CharField(max_length=255)
+    
+    email = models.EmailField(max_length=255,unique=True)
+    
+    is_active = models.BooleanField(default=True)
+    
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserAccountManager()
+    
+    USERNAME_FIELD = 'email'
+    
+    REQUIRED_FIELDS = ['name']
+    
+    def get_full_name(self):
+        return self.name
+    
+    def get_short_name(self):
+        return self.name
+    
+    def __str__(self):
+        return self.email
